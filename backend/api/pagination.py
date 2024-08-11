44a71a8f460ed90aab23abe29a987b8f8c93fab5
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-08-11 23:22:07 +0300

    Update pagination.py

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -4,9 +4,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+
 class CustomPagination(PageNumberPagination):
     """Переопределение названия поля,
     отвечающего за количество результатов в выдаче."""
 
-    PAGE_SIZE = os.getenv('PAGE_SIZE', 'PAGE_SIZE')
-    PAGE_SIZE_QUERY_PARAM = os.getenv('PAGE_SIZE_QUERY_PARAM', 'PAGE_SIZE_QUERY_PARAM')
+    PAGE_SIZE = os.getenv('PAGE_SIZE')
+    PAGE_SIZE_QUERY_PARAM = os.getenv('PAGE_SIZE_QUERY_PARAM')
