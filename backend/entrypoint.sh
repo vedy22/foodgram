5572ec1ae80ec92b3c33d54e6ad7de44a6ecf2eb
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-08-18 21:30:45 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -3,6 +3,6 @@
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --no-input
-cp -r /app/static/. /backend_static/static/
+cp -r /app/static/. /static/
 
 gunicorn foodgram.wsgi:application --bind 0.0.0.0:8000
