f06f4e878d68c4b1d12a8d6a04231d4d7d526e9c
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-08-19 20:54:44 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = "/backend_static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "backend_static")
 
 MEDIA_URL = "/backend_media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "/backend_media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "backend_media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
