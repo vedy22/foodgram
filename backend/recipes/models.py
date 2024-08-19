fde47b2292f95d6f52700ce83f71d60fd03bd73e
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-08-19 16:39:24 +0300

    Update models.py

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -22,7 +22,7 @@ class Recipe(models.Model):
     )
     image = models.ImageField(
         verbose_name="Изображение рецепта",
-        upload_to="foodgram_backend/images",
+        upload_to="backend_media",
         null=False,
     )
     text = models.TextField(verbose_name="Описание", null=False)
