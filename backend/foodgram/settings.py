19743b489ad8198b6f765193cd0eec1eb1d7bbd0
Tatyana <89541794+vedy22@users.noreply.github.com>
2024-08-19 10:37:47 +0300

    Update settings.py

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 
 STATIC_URL = "/backend_static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, "backend_static")
 
 MEDIA_URL = "/backend_media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
